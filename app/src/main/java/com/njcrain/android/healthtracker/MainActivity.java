27b0c3a0e729b5113bc032bade7f671ccbe33d0a
Nick Crain <nicholascrain@gmail.com>
2019-01-08 10:06:05 -0800

    Finger Exercises feature

--- app/src/main/java/com/njcrain/android/healthtracker/MainActivity.java
+++ app/src/main/java/com/njcrain/android/healthtracker/MainActivity.java
@@ -2,12 +2,21 @@ package com.njcrain.android.healthtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int clicks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void incrementExcercise(View v) {
+        clicks++;
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Clicked: " + clicks + " times");
     }
 }
