31556e6b6f4bf35aae2ff5f2c19e947cce24795c
Nick Crain <nicholascrain@gmail.com>
2019-01-08 13:13:56 -0800

    private variables + getters/setters

--- app/src/main/java/com/njcrain/android/healthtracker/InspirationalImage.java
+++ app/src/main/java/com/njcrain/android/healthtracker/InspirationalImage.java
@@ -1,11 +1,26 @@
 package com.njcrain.android.healthtracker;
 
 public class InspirationalImage {
-    public String src;
-    public String caption;
+    private String src;
+    private String caption;
 
     public InspirationalImage(String src, String caption) {
         this.src = src;
         this.caption = caption;
+    }
+
+    public String getSrc() {
+        return this.src;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+    public void setSrc(String src) {
+        this.src = src;
     }
 }
