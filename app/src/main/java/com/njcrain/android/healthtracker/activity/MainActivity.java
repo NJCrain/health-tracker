fa23891e34f7fc799469537b35254e75c866d751
Nick Crain <nicholascrain@gmail.com>
2019-01-17 10:03:14 -0800

    Select image from file

--- app/src/main/java/com/njcrain/android/healthtracker/activity/MainActivity.java
+++ app/src/main/java/com/njcrain/android/healthtracker/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -25,6 +26,7 @@ import androidx.core.content.FileProvider;
 
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -33,6 +35,7 @@ import com.njcrain.android.healthtracker.R;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -43,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     private static final long DELAY = 5000;
     private ImageView avatar;
     private SharedPreferences preferences;
+    private static boolean CAMERA_PERMISSION;
+    private static boolean FILES_PERMISSION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -135,7 +140,6 @@ public class MainActivity extends AppCompatActivity {
 
     //checks if the user has set their own avatar. If they haven't, display the default. If they have, display that image
     private void displayAvatar() {
-        SharedPreferences.Editor editor = preferences.edit();
         if (!preferences.contains("avatarUri")) {
             avatar.setImageResource(R.drawable.defaultuser);
         } else {
@@ -146,9 +150,30 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //---------Everything below this line includes code from https://developer.android.com/training/camera/photobasics and https://developer.android.com/training/permissions/requesting------------
-    public void updateAvatar() {
+    //Called from the select image button, starts an intent to let the user pick an image already saved on their phone
+    public void selectAvatar(View v) {
+        verifyPermissions();
+        if (FILES_PERMISSION) {
+            //This comes from https://stackoverflow.com/questions/5309190/android-pick-images-from-gallery
+            Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            getIntent.setType("image/*");
 
+            Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            pickIntent.setType("image/*");
+
+            Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+            startActivityForResult(chooserIntent, 2);
+        }
+    }
+
+    //---------Everything below this line includes code from https://developer.android.com/training/camera/photobasics and https://developer.android.com/training/permissions/requesting------------
+
+    //Called when the user want to upload a new photo. Calls the method to create a new file for that photo, then starts the intent to use the camera
+    public void updateAvatar(View v) {
+        verifyPermissions();
+    if (CAMERA_PERMISSION && FILES_PERMISSION) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -157,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 photoFile = createImageFile();
             } catch (IOException ex) {
-
+                Log.i("FileCreationError", ex.toString());
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
@@ -168,47 +193,60 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(takePictureIntent, 1);
             }
         }
+    }
 
     }
 
-    public void verifyPermissions(View v) {
+    public void verifyPermissions() {
         //Check permissions before operating, request them if needed.
         if ((ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) &&
                 (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_ID);
         } else {
-            updateAvatar();
+        CAMERA_PERMISSION =true;
+        FILES_PERMISSION = true;
         }
-
     }
 
+    //Code needed to check if the user actually granted permissions after requesting
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case REQUEST_ID: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                    updateAvatar();
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    CAMERA_PERMISSION = true;
                 } else {
-                    //TODO: find a good way to handle permissions were denied
+                CAMERA_PERMISSION = false;
+                } if (grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                    FILES_PERMISSION = true;
+                } else {
+                    FILES_PERMISSION = false;
                 }
-                return;
             }
 
         }
     }
 
+    //What to do when the activity is returned to in the case of it needed another activity to handle something
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Case of the user took a new picture for their avatar
         if (requestCode == 1 && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            avatar.setImageBitmap(imageBitmap);
+            displayAvatar();
+        }
+        //Case of user went to select a photo from saved images. Grab the proper path for that image, then save it in preferences and call displayAvatar().
+        if (requestCode == 2 && resultCode == RESULT_OK) {
+            SharedPreferences.Editor editor = preferences.edit();
+            Uri imagePath = data.getData();
+            String realPath = getRealPathFromURI(this, imagePath);
+            preferences.edit().putString("avatarUri", realPath).apply();
             displayAvatar();
         }
     }
 
+    //Used when the user wants to take a new photo for their avatar. Creates a file to save it to and sets the location to that image in preferences
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -222,5 +260,22 @@ public class MainActivity extends AppCompatActivity {
 
         preferences.edit().putString("avatarUri", image.getAbsolutePath()).apply();
         return image;
+    }
+
+    //This method come from https://stackoverflow.com/questions/3401579/get-filename-and-path-from-uri-from-mediastore
+    //It takes the URI that comes back from a selected image and transforms it into a usable path
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
     }
 }
