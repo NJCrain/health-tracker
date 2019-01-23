137e2a5a84d99ad9438cfb6f6ffa6c779595af92
Nick Crain <nicholascrain@gmail.com>
2019-01-23 13:45:16 -0800

    setup for login on main + login activity

---
+++ app/src/main/java/com/njcrain/android/healthtracker/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.njcrain.android.healthtracker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.njcrain.android.healthtracker.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
